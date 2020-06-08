879ac4da00e4bbc7b082b0cc5077ad98597d17d7
Tomi5 <tomasz.rysak@o2.pl>
2020-06-08 23:10:54 +0200

    Updates

---
+++ precache-manifest.5895f020fc7ce2f6d45016417776d404.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b2982070cb0490e7fe3156331ca7dc",
+    "url": "/tomasz-rysak-cv/index.html"
+  },
+  {
+    "revision": "7eb8fb53a484cdfd72f7",
+    "url": "/tomasz-rysak-cv/static/js/2.e4587c55.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/tomasz-rysak-cv/static/js/2.e4587c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "948d81f8a2e898494089",
+    "url": "/tomasz-rysak-cv/static/js/main.25a19d33.chunk.js"
+  },
+  {
+    "revision": "a3d24c4d18c7c3518755",
+    "url": "/tomasz-rysak-cv/static/js/runtime-main.2cbaf0d7.js"
+  },
+  {
+    "revision": "efd5778e53fa0c7e231aef8633f91037",
+    "url": "/tomasz-rysak-cv/static/media/career.efd5778e.svg"
+  },
+  {
+    "revision": "4166d86b25cb20162f7ec79f1114d12a",
+    "url": "/tomasz-rysak-cv/static/media/coding.4166d86b.svg"
+  },
+  {
+    "revision": "741977b61877cd45c3a38cf5ccea87f3",
+    "url": "/tomasz-rysak-cv/static/media/headphone.741977b6.png"
+  },
+  {
+    "revision": "205a20dd09e904d15a8de06d5f2b5e99",
+    "url": "/tomasz-rysak-cv/static/media/keyboard.205a20dd.png"
+  },
+  {
+    "revision": "b4d989acae6cba4c0be33771619424be",
+    "url": "/tomasz-rysak-cv/static/media/pen.b4d989ac.png"
+  },
+  {
+    "revision": "d4abb58f34fd41362eeae7d185576d81",
+    "url": "/tomasz-rysak-cv/static/media/plant.d4abb58f.png"
+  },
+  {
+    "revision": "2b82fe0abb39446503b67b951349718c",
+    "url": "/tomasz-rysak-cv/static/media/portrait.2b82fe0a.jpg"
+  },
+  {
+    "revision": "336097e87c5222db49944e8d5d825075",
+    "url": "/tomasz-rysak-cv/static/media/school.336097e8.svg"
+  }
+]);
--- src/LangContext.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const LangContext = React.createContext('pl');
-export const LangProvider = LangContext.Provider;
-export default LangContext;
