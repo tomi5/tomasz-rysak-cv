1387ac5a92cde99916d5c5810df7c804cea8f66a
Tomasz Rysak <trysak@asco.com.pl>
2021-07-07 09:23:00 +0200

    update projects data

--- src/datas/ProjectsData.js
+++ src/datas/ProjectsData.js
@@ -43,50 +43,50 @@ const projectsData = [
     GitHub: "https://github.com/tomi5/LandLab",
     Live: "https://landlab.pl/",
   },
-  {
-    id: 4,
-    pl: {
-      name: "Single Page - komercyjny projekt",
-      descr:
-        "Design strony i jej zakodowanie. Strona wykonana dla zaprzyjaźnionej firmy.",
-    },
-    en: {
-      name: "Single Page - commercial project",
-      descr:
-        "Website design and coding. Website prepared for a friend's company.",
-    },
-    techs: "HTML5 - CSS3 - jQuery - RWD",
-    GitHub: "https://github.com/tomi5/hydrogeopol",
-    Live: "https://tomi5.github.io/hydrogeopol/",
-  },
-  {
-    id: 5,
-    pl: {
-      name: "Szablon strony biura podróży",
-      descr: "Projekt strony stworzony w celu nauki Bootstrap 4.",
-    },
-    en: {
-      name: "Travel Service - template",
-      descr: "Bootstrap 4 training project.",
-    },
-    techs: "HTML5 - Bootstrap 4 - jQuery - LightBox",
-    GitHub: "https://github.com/tomi5/travel_service",
-    Live: "https://tomi5.github.io/travel_service/",
-  },
-  {
-    id: 6,
-    pl: {
-      name: "Single Page Template - PSD to HTML",
-      descr: "Moja pierwsza strona zakodowana z szablonu PSD.",
-    },
-    en: {
-      name: "Single Page Template - PSD to HTML",
-      descr: "My first RWD single page website converted from PSD.",
-    },
-    techs: "HTML5 - CSS3 (flexbox) - jQuery - Photoshop (slice tool)",
-    GitHub: "https://github.com/tomi5/cuda_single_pages",
-    Live: "https://tomi5.github.io/cuda_single_pages//",
-  },
+  // {
+  //   id: 4,
+  //   pl: {
+  //     name: "Single Page - komercyjny projekt",
+  //     descr:
+  //       "Design strony i jej zakodowanie. Strona wykonana dla zaprzyjaźnionej firmy.",
+  //   },
+  //   en: {
+  //     name: "Single Page - commercial project",
+  //     descr:
+  //       "Website design and coding. Website prepared for a friend's company.",
+  //   },
+  //   techs: "HTML5 - CSS3 - jQuery - RWD",
+  //   GitHub: "https://github.com/tomi5/hydrogeopol",
+  //   Live: "https://tomi5.github.io/hydrogeopol/",
+  // },
+  // {
+  //   id: 5,
+  //   pl: {
+  //     name: "Szablon strony biura podróży",
+  //     descr: "Projekt strony stworzony w celu nauki Bootstrap 4.",
+  //   },
+  //   en: {
+  //     name: "Travel Service - template",
+  //     descr: "Bootstrap 4 training project.",
+  //   },
+  //   techs: "HTML5 - Bootstrap 4 - jQuery - LightBox",
+  //   GitHub: "https://github.com/tomi5/travel_service",
+  //   Live: "https://tomi5.github.io/travel_service/",
+  // },
+  // {
+  //   id: 6,
+  //   pl: {
+  //     name: "Single Page Template - PSD to HTML",
+  //     descr: "Moja pierwsza strona zakodowana z szablonu PSD.",
+  //   },
+  //   en: {
+  //     name: "Single Page Template - PSD to HTML",
+  //     descr: "My first RWD single page website converted from PSD.",
+  //   },
+  //   techs: "HTML5 - CSS3 (flexbox) - jQuery - Photoshop (slice tool)",
+  //   GitHub: "https://github.com/tomi5/cuda_single_pages",
+  //   Live: "https://tomi5.github.io/cuda_single_pages//",
+  // },
 ];
 
 export default projectsData;
