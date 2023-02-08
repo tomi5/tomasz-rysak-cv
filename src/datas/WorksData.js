86c02e1999872b8540df773acfa8d8daeb2cc294
Tomasz <tomasz.rysak@o2.pl>
2023-02-08 11:36:14 +0100

    add jenkins and teamcity to highlighted data

--- src/datas/WorksData.js
+++ src/datas/WorksData.js
@@ -1,8 +1,56 @@
 const worksData = [
+    {
+        id: 1,
+        pl: {
+            date: "2021.09 - nadal",
+            company: "Sii Polska",
+            position: "Software engineer - B2B",
+            duties: [
+                "projektowanie i wdrażanie nowych sklepów internetowych z wykorzystaniem technologii: HTML (CSHTML), CSS (SASS, Bootstrap), JavaScript (Knockout.js),",
+                "optymalizowanie istniejącego kodu sklepów internetowych,",
+                "rozwijanie autorskiego systemu CMS,",
+                "analizowanie i implementacja wymagań klienta.",
+            ],
+        },
+        en: {
+            date: "2021.09 - ongoing",
+            company: "Sii Poland",
+            position: "Software engineer - B2B",
+            duties: [
+                "creating websites with HTML (CSHTML), CSS (SASS, Bootstrap), JavaScript (Knockout.js),",
+                "maintaining and improving existing websites,",
+                "improving a proprietary CMS",
+                "analyzing and implementing customer requirements.",
+
+//                 •	Developing a consumer facing airline booking site – 20 people team
+//                 •   Web application for the airline industry – 20 people, 2 Scrum teams
+//
+//                 •	Creating web application with VueJs, Vuex, .NET core and JavaScript,
+//                 •	Bug fixes and refactoring of existing code
+//                 •	Writing e2e tests with cypress.jo
+//
+//                     •	Creating web application with ReactJS, Redux and JavaScript,
+//                 •	Bug fixes and refactoring of existing code
+//                 •	Writing unit tests with Jest and Enzyme, and e2e test with TestCafe
+//
+//     sabre enterprise payment gateway - hosted payment page
+//
+//     •	VueJs, Vuex, CSS/SASS, C#, Cypress.jo, TeamCity, Git, Docker
+// •	    ReactJS, Redux, GraphQL, CSS/SASS, Jest, Enzyme, TestCafe, Git, Jenkins, Jest, Enyzme and TestCafe
+//
+//
+// I am working on the PWA multi-tenant e-commerce application intended mainly for the aviation industry. The platform offers all the functionality an airline needs to run its business, such as: purchasing and managing tickets, ancillaries and seats; online check-in; travel agency portal; loyalty program and more. The product can be fully personalized to airline’s needs and managed by them through dedicated Admin Portal and CMS.
+//
+// creating web application with React, Redux and Javascript,
+// ● code testing based on Jest, Enzyme and TestCafe,
+
+            ],
+        },
+    },
   {
-    id: 1,
+    id: 2,
     pl: {
-      date: "2020.11 - obecnie",
+      date: "2020.11 - 2021.09",
       company: "Asco Systemy Informatyczne",
       position: "Front-End Developer - B2B",
       duties: [
@@ -13,7 +61,7 @@ const worksData = [
       ],
     },
     en: {
-      date: "November, 2020 - ongoing",
+      date: "2020.11 - 2021.09",
       company: "Asco Systemy Informatyczne",
       position: "Front-End Developer - B2B",
       duties: [
@@ -25,7 +73,7 @@ const worksData = [
     },
   },
   {
-    id: 2,
+    id: 3,
     pl: {
       date: "2019.12 - 2020.05",
       company: "Webiso",
@@ -39,7 +87,7 @@ const worksData = [
       ],
     },
     en: {
-      date: "December, 2019 - May, 2020",
+      date: "2019.12 - 2020.05",
       company: "Webiso",
       position: "Front-End Developer - B2B",
       duties: [
@@ -52,7 +100,7 @@ const worksData = [
     },
   },
   {
-    id: 3,
+    id: 4,
     pl: {
       date: "2016.03 - 2019.11",
       company: "Orlando Travel",
@@ -66,7 +114,7 @@ const worksData = [
       ],
     },
     en: {
-      date: "March, 2016 - November, 2019",
+      date: "2016.03 - 2019.11",
       company: "Orlando Travel",
       position: "travel specialist",
       duties: [
@@ -79,7 +127,7 @@ const worksData = [
     },
   },
   {
-    id: 4,
+    id: 5,
     pl: {
       date: "2013.08 - 2016.03",
       company: "Strefa Wypraw",
@@ -92,7 +140,7 @@ const worksData = [
       ],
     },
     en: {
-      date: "August, 2013 - March, 2016",
+      date: "2013.08 - 2016.03",
       company: "Strefa Wypraw",
       position: "travel specialist",
       duties: [
